8a19effb1a9da6921c81441233e49b4ad260bad6
JelteMX <jelte.lagendijk@mendix.com>
2019-09-06 15:50:17 +0200

    :package: Get it running

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 const base = require("./node_modules/@mendix/pluggable-widgets-tools/configs/eslint.ts.base.json");
 
-base["rules"]["no-unused-expressions"] = "warn";
+// base["rules"]["no-unused-expressions"] = "warn";
 
 module.exports = {
     ...base,
